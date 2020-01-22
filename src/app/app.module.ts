4c651dc3a74846aa0c1fd79226718f960f679471
Filip Zivanov <filip0517@gmail.com>
2020-01-22 12:31:55 -0600

    made temp header/ started app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { A11yModule } from '@angular/cdk/a11y';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    A11yModule
   ],
   providers: [],
   bootstrap: [AppComponent]
